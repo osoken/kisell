d747e9f74f13a4c176651618eb11f7f2111a84f7
osoken <osoken.devel@outlook.jp>
2016-10-10 15:11:10 +0900

    setup

---
+++ kisell/__init__.py
@@ -0,0 +1,7 @@
+
+# -*- coding: utf-8 -*-
+
+__description__ = 'generator tools'
+
+__author__ = 'osoken'
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from src import __version__, __author__, __description__
+from setuptools import setup
+
+setup(
+    name='kisell',
+    version=__version__,
+    author=__author__,
+    author_email='osoken.devel@outlook.jp',
+    url='https://github.com/osoken/kisell',
+    description=__description__,
+    long_description='',
+    license='MIT',
+    packages=['kisell']
+)
