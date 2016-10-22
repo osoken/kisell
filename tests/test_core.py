395c3835093c39a7f0ca87289b0f08d40edb1bd9
osoken <osoken.devel@outlook.jp>
2016-10-22 21:20:22 +0900

    add pipe tester

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import os
 import unittest
 
 from kisell import core
+
+
+_license_file_path = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), 'LICENSE'
+)
+_license_file_content = None
+with open(_license_file_path, 'r') as f:
+    _license_file_content = f.read()
 
 
 class BaseTester(unittest.TestCase):
@@ -161,10 +170,10 @@ class OriginTester(unittest.TestCase):
         self.assertNotEqual(origin.upper(), test.upper())
         with self.assertRaises(AttributeError):
             test.non_such_attribute
-        with open(__file__, 'r') as f:
+        with open(_license_file_path, 'r') as f:
             test = core.Origin(f)
             s = test.read()
-            self.assertEqual(s[0], '#')
+            self.assertEqual(s, _license_file_content)
             test.close()
             self.assertTrue(f.closed)
 
@@ -174,10 +183,78 @@ class OriginTester(unittest.TestCase):
             self.assertEqual(res[0], 0)
             self.assertEqual(res[1], 2)
             self.assertEqual(len(res), 4)
-        fin = open(__file__, 'r')
+        fin = open(_license_file_path, 'r')
         with core.Origin(fin) as test:
             s = test.read()
-            self.assertEqual(s[0], '#')
+            self.assertEqual(s, _license_file_content)
+        self.assertTrue(fin.closed)
+
+
+class PipeTester(unittest.TestCase):
+
+    class ConcretePipe(core.Pipe):
+        def __init__(self):
+            super(PipeTester.ConcretePipe, self).__init__()
+
+        def _initialize(self):
+            for x in self.upstream:
+                yield x + 1
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test__init__(self):
+        test = PipeTester.ConcretePipe()
+        self.assertIsNone(test._Pipe__upstream)
+
+    def test__property_upstream(self):
+        test = PipeTester.ConcretePipe()
+        with self.assertRaises(core.EmptyPipeError):
+            self.assertIsNone(test.upstream)
+        p0 = PipeTester.ConcretePipe()
+        p1 = PipeTester.ConcretePipe()
+        p2 = PipeTester.ConcretePipe()
+        p0.upstream = p1
+        self.assertEqual(p1, p0.upstream)
+        p0.upstream = p2
+        self.assertEqual(p1, p0.upstream)
+        self.assertEqual(p2, p1.upstream)
+        with self.assertRaises(core.EmptyPipeError):
+            self.assertIsNone(p2.upstream)
+
+    def test__getattr__(self):
+        p0 = PipeTester.ConcretePipe()
+        p1 = PipeTester.ConcretePipe()
+        p2 = PipeTester.ConcretePipe()
+        fin = open(_license_file_path, 'r')
+        p0.upstream = p1
+        p0.upstream = p2
+        p0.upstream = fin
+        with self.assertRaises(AttributeError):
+            p0.non_such_attribute
+        self.assertEqual(p0.name, fin.name)
+        self.assertEqual(p1.name, fin.name)
+        self.assertEqual(p1.name, p0.name)
+        self.assertEqual(p2.name, fin.name)
+        self.assertEqual(p2.name, p0.name)
+        self.assertEqual(p2.name, p1.name)
+        p1.name = 'something new'
+        self.assertNotEqual(p0.name, fin.name)
+        self.assertNotEqual(p1.name, fin.name)
+        self.assertEqual(p1.name, p0.name)
+        self.assertEqual(p2.name, fin.name)
+        self.assertNotEqual(p2.name, p0.name)
+        self.assertNotEqual(p2.name, p1.name)
+
+    def test__enter__exit__(self):
+        fin = open(_license_file_path, 'r')
+        test = PipeTester.ConcretePipe()
+        test.upstream = fin
+        with test:
+            pass
         self.assertTrue(fin.closed)
 
 
