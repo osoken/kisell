6e94a564dedf71dfb79f017164723042118e9c33
osoken <osoken.devel@outlook.jp>
2016-11-04 08:16:56 +0900

    add dsv module

---
+++ kisell/dsv/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from . io import (
+    DSVParse,
+    CSVParse,
+    TSVParse,
+    DSVFormat,
+    CSVFormat,
+    TSVFormat,
+    DSVFileReader,
+    CSVFileReader,
+    TSVFileReader,
+    DSVFileWriter,
+    CSVFileWriter,
+    TSVFileWriter
+)
+
+from . import operator
